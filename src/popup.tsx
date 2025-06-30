ff741f1f8340e3578333cb5675ae76fb32d3a163
Sanish Kumar <sanish.kumar@nexthink.com>
2025-06-30 12:13:30 +0530

    chore: make basic work with tailwind

---
+++ src/popup.tsx
@@ -0,0 +1,14 @@
+import { CountButton } from "~features/count-button"
+
+import "~/src/style.css"
+
+function IndexPopup() {
+  return (
+    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
+      {/* <CountButton /> */}
+      <span className="plasmo-ml-2 plasmo-text-lg">Scrum helper</span>
+    </div>
+  )
+}
+
+export default IndexPopup
